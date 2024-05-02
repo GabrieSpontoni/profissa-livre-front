77aec36ea2a8313c750a15d528b76de05d8b103f
Gabriel Spontoni <gabriel.spontoni@mblabs.com.br>
2024-05-02 16:38:40 -0400

    feat: add new sessions

--- app/[locale]/professional/[id]/page.tsx
+++ app/[locale]/professional/[id]/page.tsx
@@ -6,32 +6,85 @@ export default function Professional() {
   const t = useTranslations("Index");
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-base-200 text-base-content">
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div>
-            <Image
-              alt=""
-              width={200}
-              height={280}
-              quality={100}
-              src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
-
-            <div className="w-48 h-48">
-              <Lottie
-                animationData={require("../../../../public/lotties/enginner.json")}
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-base-200 text-base-content">
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div>
+              <Image
+                alt=""
+                width={200}
+                height={280}
+                quality={100}
+                src="/images/gabriel-spontoni.jpg"
+                className="max-w-sm rounded-lg shadow-2xl"
               />
             </div>
-          </div>
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">{t("title")}</p>
-            <button className="btn btn-primary">Get Started</button>
+            <div>
+              <h1 className="text-5xl font-bold">Gabriel Spontoni</h1>
+              <p className="py-6">
+                Como marido de aluguel autônomo nos últimos 10 anos, adquiri uma
+                ampla gama de habilidades práticas que me permitem lidar com uma
+                variedade de tarefas de manutenção residencial. Desde reparos
+                elétricos e hidráulicos até montagem de móveis e pintura, estou
+                equipado para lidar com qualquer desafio que surgir.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="flex flex-col items-center justify-center min-h-screen text-base-content">
+        <div className="hero min-h-screen">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div>
+              <div className="w-48 h-48">
+                <Lottie
+                  animationData={require("../../../../public/lotties/money.json")}
+                />
+              </div>
+            </div>
+            <div>
+              <h1 className="text-5xl font-bold">
+                Orçamento que cabe no bolso
+              </h1>
+              <p className="py-6">
+                Seu marido de aluguel confiável e acessível está aqui para
+                resolver seus problemas domésticos. Reparos de qualidade a
+                preços justos. Deixe-me cuidar dos detalhes, para que você possa
+                relaxar e desfrutar de uma casa em perfeitas condições!
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-base-200 text-base-content">
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div>
+              <div className="w-48 h-48">
+                <Lottie
+                  animationData={require("../../../../public/lotties/enginner.json")}
+                />
+              </div>
+            </div>
+            <div>
+              <h1 className="text-5xl font-bold">
+                Ideias Inovadoras e Modernas
+              </h1>
+              <p className="py-6">
+                Descubra soluções inteligentes para os desafios domésticos de
+                hoje! Com um toque inovador e moderno, trago ideias criativas
+                para transformar sua casa. De reparos técnicos a projetos de
+                design, estou aqui para tornar sua visão uma realidade.
+                Experimente a diferença de uma abordagem fresca e inspiradora!
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
