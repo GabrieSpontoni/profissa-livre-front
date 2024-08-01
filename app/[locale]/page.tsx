769e812578125656bb618719c621abceaaebe442
Gabriel Spontoni <gabriel.spontoni@mblabs.com.br>
2024-07-31 22:37:27 -0400

    feat: banner home

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,91 +1,21 @@
-import Image from "next/image";
-import { useTranslations } from "next-intl";
-
-const propsImageCarousel: {
-  width: number;
-  height: number;
-} = {
-  width: 400,
-  height: 300,
-};
 export default function Home() {
-  const t = useTranslations("Index");
-
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-base-200 text-base-content">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <div className="rounded-t-lg overflow-hidden bg-base-200 relative min-w-96 h-64">
-            <Image
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-              fill
-              style={{
-                objectFit: "cover",
-              }}
-            />
-          </div>
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{t("title")}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+    <div
+      className="hero min-h-96"
+      style={{
+        backgroundImage:
+          "url(https://www.maridodealugueljoinville.com.br/wp-content/uploads/2015/07/banner4.jpg)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-70"></div>
+      <div className="hero-content text-neutral-content">
+        <div>
+          <h1 className="mb-5 text-5xl font-bold">
+            Encontre tudo o que precisa de serviço para sua resideência
+          </h1>
+          <p className="mb-5">Mais de 75.000 fornecedores para escolher!</p>
         </div>
       </div>
-
-      <div className="carousel rounded-box my-4 lg:mx-40">
-        <div className="carousel-item">
-          <Image
-            {...propsImageCarousel}
-            src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <Image
-            {...propsImageCarousel}
-            src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <Image
-            {...propsImageCarousel}
-            src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <Image
-            {...propsImageCarousel}
-            src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <Image
-            {...propsImageCarousel}
-            src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <Image
-            {...propsImageCarousel}
-            src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <Image
-            {...propsImageCarousel}
-            src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-            alt="Burger"
-          />
-        </div>
-      </div>
-    </main>
+    </div>
   );
 }
